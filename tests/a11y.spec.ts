1d6e991854b25f45f065e0a18f0de7e512cc842a
shreyak-zeuslearning <shreya.kathe@zeuslearning.com>
2026-06-05 19:00:14 +0530

    a11y tests

---
+++ tests/a11y.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect} from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test('Page should not have accessibility issues', async({page})=>{
+    await page.goto('https://www.saucedemo.com/');
+    const results = await new 
+    AxeBuilder({page}).analyze();
+
+    expect (results.violations.length).toBe(0);
+})
