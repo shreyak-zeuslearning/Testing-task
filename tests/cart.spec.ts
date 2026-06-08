f7870c6c41dbe417e494a75851cdb80f116355b9
shreyak-zeuslearning <shreya.kathe@zeuslearning.com>
2026-06-08 16:25:27 +0530

    cart testing added

---
+++ tests/cart.spec.ts
@@ -0,0 +1,50 @@
+import {test,expect} from '@playwright/test';
+import { users } from '../testdata/users';
+import { products } from '../testdata/products';
+ 
+const standard_user = users[0];
+const product1 = products[0];
+const product2 = products[1];
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+
+  await page.fill('[data-test="username"]', standard_user.username);
+  await page.fill('[data-test="password"]', standard_user.password);
+  await page.click('[data-test="login-button"]');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await expect(page).toHaveURL(/checkout-step-one/);
+});
+
+test("TC_010 Checkout with valid details ",async({page})=>{
+        await page.fill('[data-test="firstName"]','Jane');
+        await page.fill('[data-test="lastName"]','Doe');
+        await page.fill('[data-test="postalCode"]', '100290');
+        await page.click ('[data-test="continue"]');
+        await expect(page).toHaveURL(/checkout-step-two/);
+        await expect(page.locator('.title')).toHaveText('Checkout: Overview')
+});
+
+test("TC_011 Checkout with missing first name ",async({page})=>{
+        await page.fill('[data-test="lastName"]','Doe');
+        await page.fill('[data-test="postalCode"]', '100290');
+        await page.click ('[data-test="continue"]');
+        await expect(page.locator('[data-test="error"]')).toContainText("Error: First Name is required");
+});
+
+test("TC_012 Checkout with missing postal code ",async({page})=>{
+        await page.fill('[data-test="firstName"]','Jane');
+        await page.fill('[data-test="lastName"]','Doe');
+        await page.click ('[data-test="continue"]');
+        await expect(page.locator('[data-test="error"]')).toContainText("Error: Postal Code is required");
+});
+
+test("TC_013 Checkout with missing last name ",async({page})=>{
+        await page.fill('[data-test="firstName"]','Jane');
+        await page.fill('[data-test="postalCode"]', '100290');
+        await page.click ('[data-test="continue"]');
+        await expect(page.locator('[data-test="error"]')).toContainText("Error: Last Name is required");
+});
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,7 +5,7 @@ const standardUser= users[0];
 const lockedUser=users[1];
 
 
-test('Login page should be visible and load completely', async ({page}) =>{
+test('TC_001 Login page should be visible and load completely', async ({page}) =>{
     await page.goto('https://www.saucedemo.com/');
     await expect(page).toHaveTitle(/Swag Labs/);
 
@@ -16,7 +16,7 @@ test('Login page should be visible and load completely', async ({page}) =>{
 
 
 
-test('Valid user should login successfully', async({page}) =>{
+test('TC_002 Valid user should login successfully', async({page}) =>{
     await page.goto('https://www.saucedemo.com/');
 
     await page.fill('[data-test="username"]', standardUser.username);
@@ -26,7 +26,7 @@ test('Valid user should login successfully', async({page}) =>{
     await expect (page.locator('.title')).toHaveText('Products');
 })
 
-test('Invalid passowrd should show error', async({page})=>{
+test('TC_003 Invalid passowrd should show error', async({page})=>{
     await page.goto('https://www.saucedemo.com/');
 
     await page.fill('[data-test="username"]',standardUser.username);
@@ -36,7 +36,7 @@ test('Invalid passowrd should show error', async({page})=>{
     await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Username and password do not match any user in this service');
 })
 
-test('Locked user should not be able to login', async({page})=>{
+test('TC_004 Locked user should not be able to login', async({page})=>{
     await page.goto('https://www.saucedemo.com/');
     await page.fill('[data-test="username"]', lockedUser.username);
     await page.fill('[data-test="password"]', lockedUser.password);
